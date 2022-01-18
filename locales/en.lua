7324c5a6c127c896ea83d2f1f3fe1f542e412f92
german-admiral-man <62284136+german-admiral-man@users.noreply.github.com>
2022-01-18 20:29:59 +0100

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,9 @@
+Locales['en'] = {
+  ['valid_purchase'] = 'validate this purchase?',
+  ['yes'] = 'yes',
+  ['no'] = 'no',
+  ['not_enough_money'] = 'You do not have enough money',
+  ['press_access'] = 'Press ~INPUT_CONTEXT~ to access the ~y~Hair Salon~s~.',
+  ['barber_blip'] = 'Hair-Salon',
+  ['you_paid'] = 'you paid ~g~$%s~s~',
+}
